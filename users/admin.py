4b12d75cd2c1cbdc1e8912bb26859b9f0c0480e0
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-25 14:03:14 -0300

    added users and profile creation on admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'joined')
