fc8ec5df47c736fb73fb86f89d63cef800c30ae1
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-25 14:08:12 -0300

    added Tree creation on admin page

--- trees/admin.py
+++ trees/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Tree, PlantedTree
 
-# Register your models here.
+
+class PlantedTreeInline(admin.TabularInline):
+    model = PlantedTree
+    extra = 0
+    readonly_fields = ('user', 'age', 'planted_at', 'latitude', 'longitude')
+    can_delete = False
+    show_change_link = True
+
+
+@admin.register(Tree)
+class TreeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'scientific_name')
+    inlines = [PlantedTreeInline]
