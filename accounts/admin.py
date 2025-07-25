b4477ca379baa36f647c321938a677c1e589ad17
ja1zinh0 <joao2011bha@hotmail.com>
2025-07-25 14:01:57 -0300

    added account creation on admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'active')
+    list_filter = ('active', 'created')
+    list_editable = ('active',)
+    search_fields = ('name',)
+    filter_horizontal = ('users',)
+    readonly_fields = ('created',)
+
+    ordering = ('-created',)
--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from trees_everywhere import settings
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
 
     users = models.ManyToManyField(
